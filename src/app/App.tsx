5e49b6ac6c6aef7ef7ad76b3e5ab717b5195ad6a
eduardshovkovyi <edik.shovkovyi@gmail.com>
2022-09-21 16:58:24 +0200

    implement FSD

---
+++ src/app/App.tsx
@@ -0,0 +1,25 @@
+import React, { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import "./styles/index.scss";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+
+export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to={"/"}>Home page</Link>
+      <Link to={"/about"}>About page</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPage />} />
+          <Route path={"/"} element={<MainPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
